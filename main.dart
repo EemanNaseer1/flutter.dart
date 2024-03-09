f17e9ff87fe8e87e194692370d36c947f152fcf0
rabab72 <rababahmedmalik@gmail.com>
2024-03-09 13:09:45 +0500

    learing if else

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(){
+  int assignmentDone = 11;
+  int assignmentTotal = 20;
+  int assignmentMinimum = 14; 
+  if (assignmentDone >= assignmentMinimum){
+    print("enter in the class");
+  } else if (assignmentDone < assignmentMinimum) {
+    print("not enter in the class");
+  } else if (assignmentTotal == assignmentTotal) {
+    print("sit in front desk");
+  } else {
+    print("sit in next class");
+  }
+}
---
+++ work1.dart
@@ -0,0 +1,11 @@
+void main(){
+  int num = 23;
+
+  if(num > 0){
+    print("It is a positive number");
+  } else if (num < 0){
+    print("It is a negative number");
+  } else {
+    print("It is a zero number");
+  }
+}
