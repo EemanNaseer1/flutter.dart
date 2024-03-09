bf6ae0eb835afb26076a6c20a37bad84491dca5b
rabab72 <rababahmedmalik@gmail.com>
2024-03-09 14:03:34 +0500

    update list work

--- main.dart
+++ main.dart
@@ -5,4 +5,10 @@ void main(){
  
  List marksSheet = [78, 84, 54, 34, 98];
  print(marksSheet[0] + marksSheet[1] + marksSheet[0] + marksSheet[1] + marksSheet[1]);
+
+ print(studentId);
+ print(studentId.length);
+ print(studentId.elementAt(2));
+ studentId[0] = "45";
+ print(studentId);
 }
